650aadd0f17b6ea1e14a75562e1b33e4f2059ff1
Dror Granot <Dror@amital.co.il>
2024-04-18 16:22:31 +0300

    yay2

--- ServerLibrary/Data/AppDbContext.cs
+++ ServerLibrary/Data/AppDbContext.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ServerLibrary.Data
 {
-    public class AppDbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<Employee> Employees { get; set; }
 
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
 
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Branch> Branches { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
